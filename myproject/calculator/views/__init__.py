93e30b33d90832d8d3c6773bbb9dde211f2a0670
Conan C. Albrecht <conan@stallion.app.byu.edu>
2013-12-09 12:33:01 -0700

    moved things around for django 1.6

--- myproject/calculator/views/__init__.py
+++
@@ -1,9 +0,0 @@
-import base_app.controller
-
-
-###############################################################
-###   The templater for the calculator app.  I created it
-###   here once and then use it throughout the views
-###   within this app because we only need one.
-
-templater = base_app.controller.MakoTemplateRenderer('calculator')
