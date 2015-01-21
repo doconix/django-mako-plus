0c57d4bac03c0bff2e10a472700c67d15d413aba
Jordan <mcjoejoe@users.noreply.github.com>
2015-01-20 21:06:40 -0700

    Updated models.py template

--- django_mako_plus/controller/app_template/models.py
+++ django_mako_plus/controller/app_template/models.py
@@ -2,9 +2,9 @@ from django.db import models
 
 # Example model definition
 # ------------------------
-class Example_Model(models.Model):
-  name = models.CharField(max_length=30)
-  date_created = models.DateTimeField(auto_add_now=True)
+#class Example_Model(models.Model):
+#  name = models.CharField(max_length=30)
+#  date_created = models.DateTimeField(auto_add_now=True)
 
   
 
