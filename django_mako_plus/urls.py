a65dfa8ab1095986dc54a102f4cc1914d173bede
Conan C. Albrecht <doconix@gmail.com>
2017-01-18 10:43:07 -0700

    work

--- django_mako_plus/urls.py
+++ django_mako_plus/urls.py
@@ -2,6 +2,8 @@ from django.conf.urls import url
 
 from .router import route_request
 
+
+
 urlpatterns = [
     # /app/page.function/
     url(r'^(?P<dmp_router_app>[^/]+)/(?P<dmp_router_page>[^/]+)\.(?P<dmp_router_function>[^/\.]+)/(?P<urlparams>.*)', route_request),
