8d5caba20f67e62d979a7db6faa2951192dc96d0
Conan C. Albrecht <doconix@gmail.com>
2017-01-19 15:08:30 -0700

    small change

--- django_mako_plus/management/commands/dmp_startapp.py
+++ django_mako_plus/management/commands/dmp_startapp.py
@@ -20,74 +20,10 @@ class Command(BaseCommand):
 
 
     def handle(self, *args, **options):
-        # get the app name
-        app_name = options['appname']
-
-        # Check that the app_name doesn't already exist
-        try:
-            import_module(app_name)
-            raise CommandError("%r conflicts with the name of an existing Python module and cannot be used as an app name. Please try another name." % app_name)
-        except ImportError:
-            pass
-
-        # Check that mako is importable
-        try:
-            import mako
-        except ImportError:
-            raise CommandError("The Mako templating engine cannot be imported.  Have you installed Mako yet?")
-
-        # ensure we have a base directory
-        try:
-            if not os.path.isdir(settings.BASE_DIR):
-                raise CommandError('Your settings.py BASE_DIR setting is not a valid directory.  Please check your settings.py file for the BASE_DIR variable.')
-        except AttributeError:
-            raise CommandError('Your settings.py file is missing the BASE_DIR setting. Aborting app creation.')
-
-        # ensure we aren't overwriting an existing directory
-        app_dir = os.path.join(os.path.join(os.path.abspath(settings.BASE_DIR), app_name))
-        if os.path.exists(app_dir):
-            raise CommandError('The new app directory already exists: %s.  Aborting app creation.' % app_dir)
-
-        # create the directory structure by copying our app_template
-        os.mkdir(app_dir)
-        template_dir = os.path.join(os.path.abspath(os.path.dirname(django_mako_plus.__file__)), 'app_template')
-        def copy_dir(root):
-            for source_name in os.listdir(root):
-                # skip certain files
-                if source_name.startswith('.') or source_name == '__pycache__':
-                    continue
-
-                # calculate the filenames
-                source_path = os.path.join(root, source_name)
-                dest_path = os.path.join(app_dir, root[len(template_dir)+1:], source_name)
-
-                # if a directory, make it and then recurse
-                if os.path.isdir(source_path):
-                    os.mkdir(dest_path)
-                    copy_dir(source_path)
-
-                # if a file, copy the bytes
-                elif os.path.isfile(source_path):
-                    # read the contents
-                    fin = open(source_path)
-                    contents = fin.read()
-                    fin.close()
-                    # do the string substitution in the contents.
-                    # note I'm not using Mako templates here because escaping all the
-                    # mako sequences in the app_template files is too much.
-                    contents = contents % {
-                        'app_name': app_name,
-                        'camel_case_app_name' : ''.join([ p.capitalize() for p in app_name.split('_') ]),
-                    }
-                    # write the contents to the new file
-                    fout = open(dest_path, 'w')
-                    fout.write(contents)
-                    fout.close()
-
-        # start the copy process
-        copy_dir(template_dir)
-
-        self.stdout.write("App %s successfully created!  Don't forget to add your new app name (%s) to " % (app_name, app_name))
-        self.stdout.write("the INSTALLED_APPS list in settings.py.  Once this is done, start your server and")
-        self.stdout.write("go to http://localhost:8000/%s/index/ in a browser." % app_name)
+        self.stdout.write("Error:")
+        self.stdout.write('')
+        self.stdout.write("As of v1.7, Django-Mako-Plus apps are creted with the standard Django command.")
+        self.stdout.write("Please see https://github.com/doconix/django-mako-plus#create-a-dmp-style-app")
+        self.stdout.write('')
+        self.stdout.write("\t\t\t\t-- The Management")
 
