e171b6522e0097acc414a816244784e996890c04
Conan C. Albrecht <doconix@gmail.com>
2017-02-20 08:58:03 -0700

    version bump

--- django_mako_plus/version.py
+++ django_mako_plus/version.py
@@ -2,7 +2,7 @@
 # This allows it to import into the runtime DMP as well as
 # setup.py during installation.
 
-__version__ = '4.0.1'
+__version__ = '4.0.2'
 
 
 # Reminder on uploading to pypi and removing the build folders:
