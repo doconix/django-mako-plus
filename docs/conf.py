68b13fbf4346f87be4c05d4424eb722c037f6932
Conan C. Albrecht <doconix@gmail.com>
2017-04-07 09:41:21 -0500

    tutorial

--- docs/conf.py
+++ docs/conf.py
@@ -84,14 +84,6 @@ todo_include_todos = False
 # a list of builtin themes.
 #
 #html_theme = 'alabaster'
-import sphinx_rtd_theme
-html_theme = "sphinx_rtd_theme"
-html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
-
-def setup(app):
-    app.add_stylesheet('dmp.css')
-
-
 html_show_sourcelink = False
 html_show_sphinx = False
 
@@ -105,6 +97,27 @@ html_show_sphinx = False
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+# on_rtd is whether we are on readthedocs.org, this line of code grabbed from docs.readthedocs.org
+import os
+on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
+
+if not on_rtd:  # only import and set the theme if we're building docs locally
+    import sphinx_rtd_theme
+    html_theme = 'sphinx_rtd_theme'
+    html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+    # Override default css to get a larger width for local build
+    def setup(app):
+        app.add_stylesheet('dmp.css')
+else:
+    # Override default css to get a larger width for ReadTheDoc build
+    html_context = {
+        'css_files': [
+            'https://media.readthedocs.org/css/sphinx_rtd_theme.css',
+            'https://media.readthedocs.org/css/readthedocs-doc-embed.css',
+            '_static/dmp.css',
+        ],
+    }
 
 
 # -- Options for HTMLHelp output ------------------------------------------
