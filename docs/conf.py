b9568759f68e714adaf617d9c335c1ad2b06ecfd
Conan C. Albrecht <doconix@gmail.com>
2017-04-07 10:25:27 -0500

    tutorial

--- docs/conf.py
+++ docs/conf.py
@@ -101,21 +101,20 @@ html_static_path = ['_static']
 # on_rtd is whether we are on readthedocs.org, this line of code grabbed from docs.readthedocs.org
 import os
 on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
-
-if not on_rtd:  # only import and set the theme if we're building docs locally
+if not on_rtd:
     import sphinx_rtd_theme
     html_theme = 'sphinx_rtd_theme'
     html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
-    # Override default css to get a larger width for local build
     def setup(app):
-        app.add_stylesheet('theme_overrides.css')
+        app.add_stylesheet('dmp.css')
 else:
-    # Override default css to get a larger width for ReadTheDoc build
+    def setup(app):
+        app.add_stylesheet('dmp.css')
     html_context = {
         'css_files': [
             'https://media.readthedocs.org/css/sphinx_rtd_theme.css',
             'https://media.readthedocs.org/css/readthedocs-doc-embed.css',
-            '_static/theme_overrides.css',
+            '_static/dmp.css',
         ],
     }
 
