de720c260e5328a25cbf3e9021bf5f452b4f16d4
Conan C. Albrecht <doconix@gmail.com>
2017-11-04 22:03:52 -0600

    sass back fully

--- django_mako_plus/sass.py
+++ django_mako_plus/sass.py
@@ -128,6 +128,73 @@ def compile_scssm_file(scssm_file, css_file):
             os.utime(css_file)
 
 
-#############################
-### Utilities
+
+#################################################################
+###   File locking context manager - used by sass.py
+
+class lock_file(object):
+    '''
+    A context manager that provides a non-blocking file lock on both Unix and Windows.
+    Uses a temporary file for the lock - in the same directory as the filename.
+    If the lock fails after timeout_seconds, an OSError is raised.
+
+    I'm not using fcntl.flock because it is Unix-only and DMP is used on both
+    Unix and Windows.  This is a very simplistic locking scheme, but it works
+    for our purposes.
+    '''
+    def __init__(self, filename, timeout_seconds=5):
+        self.filename = filename
+        self.lock_filename = '{}.templock'.format(filename)
+        self.timeout_seconds = timeout_seconds
+
+
+    def __enter__(self):
+        # acquire the lock to it
+        for i in range(self.timeout_seconds):
+            # create the lock file
+            if not os.path.exists(self.lock_filename):
+                self.fd = open(self.lock_filename, 'w')
+                # we created successfully, so break out!
+                break
+            else:  # couldn't get the lock, so wait a second a try again
+                time.sleep(1)
+        else:  # we couldn't get a lock in timeout_seconds tries, so raise the exception
+            raise OSError("Unable to acquire lock on {}; if you are sure no other processes are running, you may need to delete the lock file manually: {}".format(self.filename, self.lock_filename))
+        # return
+        return self
+
+
+    def __exit__(self, exec_type, exec_val, exec_tb):
+        # close and remove the temporary file
+        self.fd.close()
+        try:
+            os.remove(self.lock_filename)
+        except FileNotFoundError:  # shouldn't ever happen, but just in case
+            pass
+
+
+
+
+
+######################################################
+###   Encoding routines.
+###   Using Base32 because its alphabet conforms to
+###   CSS selectors.
+
+def encode32(st):
+    '''Encodes the given string to base64.'''
+    if not isinstance(st, bytes):
+        st = st.encode('utf8')              # we now have a byte string rather than the original unicode text
+    b32_byte_st = base64.b32encode(st)   # we now have a base32-encoded byte string representing the text
+    return b32_byte_st.decode('ascii').replace('=', '9')    # we're now back to Unicode (using ascii decoding since base32 is all ascii characters)
+
+
+def decode32(st):
+    '''Decodes the given base64-encoded string.'''
+    st = st.replace('9', '=')
+    if not isinstance(st, bytes):
+        st = st.encode('ascii')     # we now have a byte string of the base64-encoded text instead of the Unicode base32s-encoded st
+    byte_st = base64.b32decode(st)   # we now have a byte string of the original text
+    return byte_st.decode('utf8')         # we now have a Unicode string of the original text
+
 
