09b5b56302086bac4613bdc87381882d1e4c16b3
Conan C. Albrecht <doconix@gmail.com>
2018-06-11 23:16:29 -0600

    all tests run again

--- tests_project/homepage/tests/test_urls.py
+++ tests_project/homepage/tests/test_urls.py
@@ -10,88 +10,88 @@ class Tester(TestCase):
         self.assertEqual(resp.status_code, 200)
 
     def test_app_page_function_urlparams(self):
-        PATTERN_NAME = 'DMP /app/page.function/urlparams'
+        PATTERN_NAME = 'DMP /app/page.function/urlparams for homepage'
         resp = self.client.get('/homepage/index.process_request/1/2/3/')
         self.assertEqual(resp.status_code, 200)
         self.assertEqual(resp.wsgi_request.resolver_match.url_name, PATTERN_NAME)
         resp = self.client.get('/homepage/index.process_request/1/2/3')
         self.assertEqual(resp.status_code, 200)
         self.assertEqual(resp.wsgi_request.resolver_match.url_name, PATTERN_NAME)
+
+    def test_app_page_function(self):
+        PATTERN_NAME = 'DMP /app/page.function for homepage'
         resp = self.client.get('/homepage/index.process_request/')
         self.assertEqual(resp.status_code, 200)
         self.assertEqual(resp.wsgi_request.resolver_match.url_name, PATTERN_NAME)
-
-    def test_app_page_function(self):
-        PATTERN_NAME = 'DMP /app/page.function'
         resp = self.client.get('/homepage/index.process_request')
         self.assertEqual(resp.status_code, 200)
         self.assertEqual(resp.wsgi_request.resolver_match.url_name, PATTERN_NAME)
 
     def test_app_page_urlparams(self):
-        PATTERN_NAME = 'DMP /app/page/urlparams'
+        PATTERN_NAME = 'DMP /app/page/urlparams for homepage'
         resp = self.client.get('/homepage/index/1/2/3/')
         self.assertEqual(resp.status_code, 200)
         self.assertEqual(resp.wsgi_request.resolver_match.url_name, PATTERN_NAME)
         resp = self.client.get('/homepage/index/1/2/3')
         self.assertEqual(resp.status_code, 200)
         self.assertEqual(resp.wsgi_request.resolver_match.url_name, PATTERN_NAME)
+
+    def test_app_page(self):
+        PATTERN_NAME = 'DMP /app/page for homepage'
         resp = self.client.get('/homepage/index/')
         self.assertEqual(resp.status_code, 200)
         self.assertEqual(resp.wsgi_request.resolver_match.url_name, PATTERN_NAME)
-
-    def test_app_page(self):
-        PATTERN_NAME = 'DMP /app/page'
         resp = self.client.get('/homepage/index')
         self.assertEqual(resp.status_code, 200)
         self.assertEqual(resp.wsgi_request.resolver_match.url_name, PATTERN_NAME)
 
     def test_app(self):
-        PATTERN_NAME = 'DMP /app'
-        resp = self.client.get('/homepage')
+        PATTERN_NAME = 'DMP /app for homepage'
+        resp = self.client.get('/homepage/')
         self.assertEqual(resp.status_code, 200)
         self.assertEqual(resp.wsgi_request.resolver_match.url_name, PATTERN_NAME)
-        resp = self.client.get('/homepage/')
+        resp = self.client.get('/homepage')
         self.assertEqual(resp.status_code, 200)
         self.assertEqual(resp.wsgi_request.resolver_match.url_name, PATTERN_NAME)
 
     def test_page_function_urlparams(self):
-        PATTERN_NAME = 'DMP /page.function/urlparams'
+        PATTERN_NAME = 'DMP /app/page.function/urlparams for <default app>'
         resp = self.client.get('/index.process_request/1/2/3/')
         self.assertEqual(resp.status_code, 200)
         self.assertEqual(resp.wsgi_request.resolver_match.url_name, PATTERN_NAME)
         resp = self.client.get('/index.process_request/1/2/3')
         self.assertEqual(resp.status_code, 200)
         self.assertEqual(resp.wsgi_request.resolver_match.url_name, PATTERN_NAME)
+
+    def test_page_function(self):
+        PATTERN_NAME = 'DMP /app/page.function for <default app>'
         resp = self.client.get('/index.process_request/')
         self.assertEqual(resp.status_code, 200)
         self.assertEqual(resp.wsgi_request.resolver_match.url_name, PATTERN_NAME)
-
-    def test_page_function(self):
-        PATTERN_NAME = 'DMP /page.function'
         resp = self.client.get('/index.process_request')
         self.assertEqual(resp.status_code, 200)
         self.assertEqual(resp.wsgi_request.resolver_match.url_name, PATTERN_NAME)
 
     def test_page_urlparams(self):
-        PATTERN_NAME = 'DMP /page/urlparams'
+        PATTERN_NAME = 'DMP /app/page/urlparams for <default app>'
         resp = self.client.get('/index/1/2/3/')
         self.assertEqual(resp.status_code, 200)
         self.assertEqual(resp.wsgi_request.resolver_match.url_name, PATTERN_NAME)
         resp = self.client.get('/index/1/2/3')
         self.assertEqual(resp.status_code, 200)
         self.assertEqual(resp.wsgi_request.resolver_match.url_name, PATTERN_NAME)
+
+    def test_page(self):
+        PATTERN_NAME = 'DMP /app/page for <default app>'
         resp = self.client.get('/index/')
         self.assertEqual(resp.status_code, 200)
         self.assertEqual(resp.wsgi_request.resolver_match.url_name, PATTERN_NAME)
-
-    def test_page(self):
-        PATTERN_NAME = 'DMP /page'
         resp = self.client.get('/index')
         self.assertEqual(resp.status_code, 200)
         self.assertEqual(resp.wsgi_request.resolver_match.url_name, PATTERN_NAME)
 
     def test_root(self):
-        PATTERN_NAME = 'DMP /'
+        PATTERN_NAME = 'DMP /app for <default app>'
         resp = self.client.get('/')
         self.assertEqual(resp.status_code, 200)
         self.assertEqual(resp.wsgi_request.resolver_match.url_name, PATTERN_NAME)
