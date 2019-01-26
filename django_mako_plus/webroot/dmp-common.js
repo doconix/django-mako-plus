4ee0a12d79c0c20892a97aef51eaadd04608be75
Conan C. Albrecht <doconix@gmail.com>
2019-01-25 19:39:25 -0700

    version

--- django_mako_plus/webroot/dmp-common.js
+++ django_mako_plus/webroot/dmp-common.js
@@ -10,7 +10,7 @@
 
     // connect the dmp object
     window.DMP_CONTEXT = {
-        __version__: '5.9.1',   // DMP version to check for mismatches
+        __version__: '5.9.2',   // DMP version to check for mismatches
         contexts: {},           // id -> context1
         contextsByName: {},     // app/template -> [ context1, context2, ... ]
         lastContext: null,      // last inserted context (see getAll() below)
